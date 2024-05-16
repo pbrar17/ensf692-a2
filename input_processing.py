3369514afe589a58eab5fd80084c831a092d4256
Pahul Brar <pahulbrar17@gmail.com>
2024-05-16 10:28:19 -0600

    Finished with the main bulk of the code

--- input_processing.py
+++ input_processing.py
@@ -1,5 +1,5 @@
 # input_processing.py
-# YOUR NAME, ENSF 692 P24
+# Pahul Brar, ENSF 692 P24
 # A terminal-based program for processing computer vision changes detected by a car.
 # Detailed specifications are provided via the Assignment 2 README file.
 # You must include the code provided below but you may delete the instructional comments.
@@ -7,6 +7,7 @@
 # You may import any modules from the standard Python library.
 # Remember to include your name and comments.
 
+import sys
 
 
 # No global variables are permitted
@@ -18,17 +19,27 @@ class Sensor:
     # Must include a constructor that uses default values
     # You do not need to provide commenting above the constructor
     def __init__(self):
-        pass
+        self.light = "green"
+        self.pedestrian = "no"
+        self.vehicle = "no"
 
     # Replace these comments with your function commenting
-    def update_status(): # You may decide how to implement the arguments for this function
-        pass
+    def update_status(self, light = None, pedestrian = None, vehicle = None): # You may decide how to implement the arguments for this function
+        if light is not None:
+            self.light = light
+        elif pedestrian is not None:
+            self.pedestrian = pedestrian
+        elif vehicle is not None:
+            self.vehicle = vehicle
+        else:
+            pass
 
 
 
 # The sensor object should be passed to this function to print the action message and current status
 # Replace these comments with your function commenting
 def print_message(sensor):
+    print("Light = " + sensor.light + ", Pedestarian = " + sensor.pedestrian + ", Vehicle = " + sensor.vehicle)
     pass
 
 
@@ -36,9 +47,41 @@ def print_message(sensor):
 # Complete the main function below
 def main():
     print("\n***ENSF 692 Car Vision Detector Processing Program***\n")
+    
+while(True):
+    mySensor = Sensor()
+    while(True):
+        print("Are Changes Detected in the Vision Input")
+        try:
+            selection = int(input("Select 1 for light, 2 for pedestarian, 3 for vehicle,or 0 to end the program: "))
+            if(selection not in [0,1,2,3]):
+                raise ValueError("You did not select a valid entry, please try again")
+        except ValueError as e:
+            print(e)
+            continue
+        if (selection == 0):
+            print("Exiting Program")
+            sys.exit()
+        break
+    while(True):
+        try:
+            change = input("What change has been identified?: ")
+            if(((selection == 2 or selection == 3) and (change not in ['yes', 'no']) )or (selection == 1 and change not in ['red', 'yellow', 'green'])):
+                raise ValueError("you did not provide a valid change please try again")
+        except ValueError as e:
+            print(e)
+            continue
+        if(selection == 1):
+            mySensor.update_status(light = change)
+            break
+        elif(selection == 2):
+            mySensor.update_status(pedestrian = change)
+            break
+        elif(selection == 3):
+            mySensor.update_status(vehicle = change)
+            break
 
-
-
+    print_message(mySensor)
 
 
 # Conventional Python code for running main within a larger program
