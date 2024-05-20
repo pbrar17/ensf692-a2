4c458d6cf370c5d0b26c4df52deda2770ee49473
Pahul Brar <pahulbrar17@gmail.com>
2024-05-20 16:02:06 -0700

    added commenting, that explains what heach function does, also fixed the memory issue

--- input_processing.py
+++ input_processing.py
@@ -14,30 +14,30 @@ import sys
 
 
 # You do not need to provide additional commenting above this class, just the user-defined functions within the class
+# A class to represent the sensor status of the car's vision system.
 class Sensor:
 
     # Must include a constructor that uses default values
     # You do not need to provide commenting above the constructor
+    # Initializes the Sensor object with default values.
     def __init__(self):
         self.light = "green"
         self.pedestrian = "no"
         self.vehicle = "no"
 
-    # Replace these comments with your function commenting
+    # The update function is passed the update values, and they are by default set to None, so they won't update unless user provides an update.
+    # if something is update, then the value is changed for the object.
     def update_status(self, light = None, pedestrian = None, vehicle = None): # You may decide how to implement the arguments for this function
         if light is not None:
             self.light = light
-        elif pedestrian is not None:
+        if pedestrian is not None:
             self.pedestrian = pedestrian
-        elif vehicle is not None:
+        if vehicle is not None:
             self.vehicle = vehicle
-        else:
-            pass
-
 
 
 # The sensor object should be passed to this function to print the action message and current status
-# Replace these comments with your function commenting
+# Given the sensor object, based on the preset conditions, on the value of the sensor, a corresponding message is printed.
 def print_message(sensor):
     if(sensor.light == 'red' or sensor.pedestrian == "yes" or sensor.vehicle == "yes"):
         print('')
@@ -52,48 +52,49 @@ def print_message(sensor):
         print("Caution")
         print('')
     print("Light = " + sensor.light + ", Pedestarian = " + sensor.pedestrian + ", Vehicle = " + sensor.vehicle)
+    print('')
     pass
 
-
-
-# Complete the main function below
+# Main function to run the car vision detector processing program.
 def main():
     print("\n***ENSF 692 Car Vision Detector Processing Program***\n")
     
-while(True):
     mySensor = Sensor()
     while(True):
-        print("Are Changes Detected in the Vision Input")
-        try:
-            selection = int(input("Select 1 for light, 2 for pedestarian, 3 for vehicle,or 0 to end the program: "))
-            if(selection not in [0,1,2,3]):
-                raise ValueError("You did not select a valid entry, please try again")
-        except ValueError as e:
-            print(e)
-            continue
-        if (selection == 0):
-            print("Exiting Program")
-            sys.exit()
-        break
-    while(True):
-        try:
-            change = input("What change has been identified?: ")
-            if(((selection == 2 or selection == 3) and (change not in ['yes', 'no']) )or (selection == 1 and change not in ['red', 'yellow', 'green'])):
-                raise ValueError("you did not provide a valid change please try again")
-        except ValueError as e:
-            print(e)
-            continue
-        if(selection == 1):
-            mySensor.update_status(light = change)
+        while(True):
+            print("Are Changes Detected in the Vision Input")
+            try:
+                selection = int(input("Select 1 for light, 2 for pedestarian, 3 for vehicle,or 0 to end the program: "))
+                if(selection not in [0,1,2,3]):
+                    raise ValueError("You did not select a valid entry, please try again")
+            except ValueError as e:
+                print(e)
+                print(' ')
+                continue
+            if (selection == 0):
+                print("Exiting Program")
+                sys.exit()
             break
-        elif(selection == 2):
-            mySensor.update_status(pedestrian = change)
-            break
-        elif(selection == 3):
-            mySensor.update_status(vehicle = change)
-            break
+        while(True):
+            try:
+                change = input("What change has been identified?: ")
+                if(((selection == 2 or selection == 3) and (change not in ['yes', 'no']) )or (selection == 1 and change not in ['red', 'yellow', 'green'])):
+                    raise ValueError("you did not provide a valid change, please try again")
+            except ValueError as e:
+                print(e)
+                print(' ')
+                continue
+            if(selection == 1):
+                mySensor.update_status(light = change)
+                break
+            elif(selection == 2):
+                mySensor.update_status(pedestrian = change)
+                break
+            elif(selection == 3):
+                mySensor.update_status(vehicle = change)
+                break
 
-    print_message(mySensor)
+        print_message(mySensor)
 
 
 # Conventional Python code for running main within a larger program
